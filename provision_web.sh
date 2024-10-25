47a9be215de0dfe26401d4596d8fb473d2fed668
AlbertoPerdomocarpio <71129251+AlbertoPerdomocarpio@users.noreply.github.com>
2024-10-25 15:49:07 +0200

    Update

--- provision_web.sh
+++ provision_web.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Aggiornamento del sistema
-sudo apt-get update -y
+apt-get update -y
 
 # Installazione di Apache e PHP esentioni
-sudo apt-get install -y apache2 php libapache2-mod-php php-mysql php-mysqli
+apt-get install -y apache2 php libapache2-mod-php php-mysql php-mysqli
 
 # Abilitazione e avvio di Apache
-sudo systemctl enable apache2
-sudo systemctl start apache2
+systemctl enable apache2
+systemctl start apache2
 
 # Installazione di Adminer per gestione database
-sudo mkdir -p /var/www/html/adminer
-sudo wget -q -O /var/www/html/adminer/index.php https://www.adminer.org/latest.php
+mkdir -p /var/www/html/adminer
+wget -q -O /var/www/html/adminer/index.php https://www.adminer.org/latest.php
 
 # Impostazione dei permessi
-sudo chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html
 
 # Riavvio di Apache per assicurarsi che tutte le modifiche siano attive
-sudo systemctl restart apache2
+systemctl restart apache2
